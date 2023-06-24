d31f6d50b5ff84e7eddc9d747d4e1c309042e4f0
Ksaveras Sakys <xawiers@gmail.com>
2023-06-24 19:42:15 +0100

    ci: Update dev dependencies, introduce rector

---
+++ rector.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+/*
+ * This file is part of ksaveras/guzzle-circuit-breaker-middleware.
+ *
+ * (c) Ksaveras Sakys <xawiers@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
+use Rector\CodingStyle\Rector\ClassConst\VarConstantCommentRector;
+use Rector\CodingStyle\Rector\ClassMethod\NewlineBeforeNewAssignSetRector;
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ]);
+
+    // define sets of rules
+    $rectorConfig->sets([
+        SetList::PHP_81,
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::DEAD_CODE,
+        SetList::PRIVATIZATION,
+        SetList::EARLY_RETURN,
+        SetList::INSTANCEOF,
+        SetList::TYPE_DECLARATION,
+    ]);
+
+    $rectorConfig->skip([
+        FlipTypeControlToUseExclusiveTypeRector::class,
+        NewlineBeforeNewAssignSetRector::class,
+        VarConstantCommentRector::class,
+    ]);
+};
